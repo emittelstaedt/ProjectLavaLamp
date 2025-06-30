bfb740f1a4fcf640aca6e088f57a1fe10b0a0d7b
Tavin Yorgason <tazasor@gmail.com>
2025-06-30 12:55:41 -0700

    Fix crouching under objects bug
    Before, if the player uncrouched under an object they had to crouch to get under, they would clip into the the object. Now, the player can't uncrouch if there is something too close above it.

--- Assets/Scripts/Player/States/CrouchState.cs
+++ Assets/Scripts/Player/States/CrouchState.cs
@@ -14,14 +14,15 @@ public class CrouchState : PlayerState
     {
         Vector2 moveValue = player.PlayerInputs.moveAction.ReadValue<Vector2>();
         Vector3 moveDirection = player.CalculateMoveDirection(moveValue);
+        bool hasRoomToUncrouch = HasRoomToUncrouch();
 
-        if (!player.PlayerInputs.crouchAction.IsPressed())
+        if (!player.PlayerInputs.crouchAction.IsPressed() && hasRoomToUncrouch)
         {
             player.SwitchState(moveValue.magnitude > 0.1f ? new WalkState(player) : new IdleState(player));
             return;
         }
 
-        if (player.PlayerInputs.jumpAction.IsPressed() && player.IsGrounded())
+        if (player.PlayerInputs.jumpAction.IsPressed() && player.IsGrounded() && hasRoomToUncrouch)
         {
             player.SwitchState(new JumpState(player));
         }
@@ -34,5 +35,21 @@ public class CrouchState : PlayerState
     {
         player.IsCrouching = false;
         player.TargetHeight = player.PlayerStats.NormalHeight;
+    }
+    
+    private bool HasRoomToUncrouch()
+    {
+        float radius = player.CharacterController.radius;
+        
+        float distanceToBottomSphere = (player.CharacterController.height / 2) - radius;
+        float worldDistanceToBottomSphere = distanceToBottomSphere * player.transform.localScale.y;
+        
+        float distanceToTopSphere = player.PlayerStats.NormalHeight - (player.CharacterController.height / 2) - radius;
+        float worldDistanceToTopSphere = distanceToTopSphere * player.transform.localScale.y;
+        
+        Vector3 bottomSphereCenter = player.transform.position - Vector3.up * worldDistanceToBottomSphere;
+        Vector3 topSphereCenter = player.transform.position + Vector3.up * worldDistanceToTopSphere;
+        
+        return !Physics.CheckCapsule(bottomSphereCenter, topSphereCenter, radius);
     }
 }
