618d31a18048be93d7dd5c255ad11c04750ebcb6
Aiden Shepard <aidenL.shepard@gmail.com>
2025-07-09 14:24:54 -0700

    Rename generic to general in comment
    Generic refers to a base template while general implies a general use
    case. Updated the comment to reflect that.

--- Assets/Scripts/Events/Subscribers/StringEventChannelSubscriber.cs
+++ Assets/Scripts/Events/Subscribers/StringEventChannelSubscriber.cs
@@ -1,5 +1,5 @@
 /// <summary>
-/// Generic event channel subscriber that listens and receives string payload.
+/// General event channel subscriber that listens and receives string payload.
 /// </summary>
 public class StringEventChannelSubscriber : GenericEventChannelSubscriber<string, StringEventChannelSO>
 {
