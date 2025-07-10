8be22e1565da45142c58cd5a10ddf36ac3059ef9
Tavin Yorgason <tazasor@gmail.com>
2025-07-09 17:16:24 -0700

    Create Vector2 general event SO and subscriber

---
+++ Assets/Scripts/Events/ScriptableObjects/Vector2EventChannelSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// General event channel that broadcasts and carries Vector2 payload.
+/// </summary>
+[CreateAssetMenu(menuName = "Events/Vector2 Event Channel", fileName = "Vector2EventChannel")]
+public class Vector2EventChannelSO : GenericEventChannelSO<Vector2>
+{
+}
---
+++ Assets/Scripts/Events/Subscribers/Vector2EventChannelSubscriber.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+/// <summary>
+/// General event channel subscriber that listens and receives Vector2 payload.
+/// </summary>
+public class Vector2EventChannelSubscriber : GenericEventChannelSubscriber<Vector2, Vector2EventChannelSO>
+{
+}
