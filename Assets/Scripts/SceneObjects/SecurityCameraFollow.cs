7b312c575ea74bf750678588860b55cea160b856
NikNowak <nowa9219@vandals.uidaho.edu>
2025-07-16 09:02:50 -0700

    Script Style Edit
    edited the script and took out white space and some error checking to fit the style guide.

--- Assets/Scripts/SceneObjects/SecurityCameraFollow.cs
+++ Assets/Scripts/SceneObjects/SecurityCameraFollow.cs
@@ -3,33 +3,19 @@ using UnityEngine;
 public class SecurityCameraFollow : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 2f;
-
-       private readonly Quaternion rotationOffset = Quaternion.Euler(-90f, 180f, 0f);
-
+    private readonly Quaternion rotationOffset = Quaternion.Euler(-90f, 180f, 0f);
     private Transform mainCameraTransform;
 
-    private void Start()
+    private void Awake()
     {
-
-        if (Camera.main != null)
-        {
-            mainCameraTransform = Camera.main.transform;
-        }
-        else
-        {
-            Debug.LogError("No Main Camera found! Set your camera's tag to 'MainCamera'.");
-        }
+        mainCameraTransform = Camera.main.transform;
     }
 
     private void LateUpdate()
     {
-        if (mainCameraTransform == null) return;
-
         Vector3 directionToCamera = mainCameraTransform.position - transform.position;
 
-
         Quaternion targetRotation = Quaternion.LookRotation(directionToCamera, Vector3.up) * rotationOffset;
-
 
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
