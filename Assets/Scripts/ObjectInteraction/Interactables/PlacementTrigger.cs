614a23fa29764ab50f8cc9d353b2e4c626f7f115
Aiden Shepard <aidenL.shepard@gmail.com>
2025-07-17 15:09:29 -0700

    Prevent placement container clipping into walls
    If a placement point is inside of another collider when the item is placed there then it will get the distance needed to move it towards the base item it is being placed on. Updated this to be more
    adaptable to any environment you're building in.

--- Assets/Scripts/ObjectInteraction/Interactables/PlacementTrigger.cs
+++ Assets/Scripts/ObjectInteraction/Interactables/PlacementTrigger.cs
@@ -7,13 +7,17 @@ public class PlacementTrigger : MonoBehaviour, IInteractable
     [SerializeField] private string requiredItem;
     [SerializeField] private Transform placementContainer;
     [SerializeField] private InteractableSettingsSO Settings;
+    private LayerMask ignoreCollisionLayer;
     private Transform placementNode;
     private Outline outline;
     private GameObject currentItemHeld;
     private GameObject lastItemheld;
+    private readonly Collider[] potentialHits = new Collider[10];
 
     private void Awake()
     {
+        ignoreCollisionLayer = ~(1 << LayerMask.NameToLayer("IgnoreItemCollision"));
+
         placementNode = transform.parent;
 
         outline = GetComponent<Outline>();
@@ -66,22 +70,53 @@ public class PlacementTrigger : MonoBehaviour, IInteractable
 
         parentMeshFilter.sharedMesh = AddChildMeshToParent(parentMeshFilter, itemMeshFilter);
 
-        // Renderer bounds are world based rather than MeshFilter's local based bounds.
-        Renderer itemRenderer = lastItemheld.GetComponent<Renderer>();
-        Vector3 bottom = placementContainer.position + Vector3.down * (itemRenderer.bounds.extents.y / 2);
+        // Move the object if placement container is inside another collider in the direction of the base object.
+        MeshCollider meshCollider = lastItemheld.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = itemMeshFilter.sharedMesh;
+        meshCollider.convex = true;
 
-        // Reverse ray in case origin would be in the ground and not hit it.
-        Ray ray = new(bottom, Vector3.up);
-        ray.origin = ray.GetPoint(parentMeshFilter.sharedMesh.bounds.extents.y);
-        ray.direction = -ray.direction;
+        int hitCount = Physics.OverlapSphereNonAlloc
+        (
+            meshCollider.transform.position,
+            meshCollider.bounds.extents.magnitude, 
+            potentialHits, 
+            ignoreCollisionLayer
+        );
 
-        if (Physics.Raycast(ray, out RaycastHit hit, 100f, LayerMask.GetMask("Ground")))
+        for (int i = 0; i < hitCount; i++)
         {
-            if (bottom.y < hit.point.y)
+            Collider potentialHit = potentialHits[i];
+
+            if (potentialHit.isTrigger)
             {
-                placementNode.parent.position += new Vector3(0f, Vector3.Distance(bottom, hit.point), 0f);
+                continue;
+            }
+
+            bool doesPenetrate = Physics.ComputePenetration
+            (
+                meshCollider, meshCollider.transform.position, meshCollider.transform.rotation,
+                potentialHit, potentialHit.transform.position, potentialHit.transform.rotation,
+                out _, out _
+            );
+
+            if (doesPenetrate)
+            {
+                Renderer lastItemRenderer = lastItemheld.GetComponent<Renderer>();
+                Bounds itemBounds = lastItemRenderer.bounds;
+                Vector3 baseItemPosition = placementNode.parent.position;
+
+                Vector3 closestPoint = potentialHit.ClosestPointOnBounds(baseItemPosition);
+                Vector3 directionToMove = (closestPoint - baseItemPosition).normalized;
+
+                float distanceToBounds = Vector3.Scale(directionToMove, itemBounds.extents).magnitude;
+                Vector3 farthestPoint = itemBounds.center + directionToMove * distanceToBounds;
+
+                float distanceToMove = Vector3.Dot(closestPoint - farthestPoint, directionToMove);
+
+                placementNode.parent.position += directionToMove * distanceToMove;
             }
         }
+        DestroyImmediate(meshCollider);
 
         // Once an item has been placed the placement point is no longer needed so stop interacting with it.
         stopInteraction.RaiseEvent();
