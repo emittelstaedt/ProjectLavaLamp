d5c2dda59a895d696ac6a212b6754e46ed9aa8e4
AmandaBoard <boar9227@vandals.uidaho.edu>
2025-07-28 12:43:37 -0700

    Update Audio Manager and Add Tag/AudioListener
    The audio manager now has all the clips stored in its prefab with the
    clip name matching the type name provided in SoundType.cs. For the
    player to hear the sounds, I attached an AudioListener component to the
    player prefab. An additional change to the MainRoom prefab and TagManager
    was adding a "Ground" tag and applying that tag to the ground objects in
    the MainRoom Prefab in order for objects to properly play the drop sound
    when colliding with the ground.

--- Assets/Scripts/Audio/SoundType.cs
+++ Assets/Scripts/Audio/SoundType.cs
@@ -1,4 +1,18 @@
 public enum SoundType
 {
     Unknown,
+    Walk,
+    Sprint,
+    Land,
+    DoorOpen,
+    DoorClose,
+    ItemCombine,
+    ItemDrop,
+    CoffeeSlurp,
+    CoffeeBrew,
+    Alarm,
+    Success,
+    InChute,
+    OutChute,
+    MessageNotification
 };
