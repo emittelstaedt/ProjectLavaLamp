d41787274567233b0050abd9f2d26f67e9790bd0
AmandaBoard <boar9227@vandals.uidaho.edu>
2025-07-29 09:46:47 -0700

    Remove white space and unnecessary header

--- Assets/Scripts/ObjectInteraction/Interactables/PickupItem.cs
+++ Assets/Scripts/ObjectInteraction/Interactables/PickupItem.cs
@@ -90,7 +90,6 @@ public class PickupItem : MonoBehaviour, IInteractable
                 else
                 {
                     dropItem.RaiseEvent();
-
                     break;
                 }
             }
