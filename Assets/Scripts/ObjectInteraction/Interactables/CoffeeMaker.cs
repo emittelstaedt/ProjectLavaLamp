e08464ad8bd38eef5183c510619d7bdc9ebcd055
Aiden Shepard <aidenL.shepard@gmail.com>
2025-07-30 17:26:14 -0700

    Have coffee cup be placed in coffee maker
    Upon interacting with the coffee maker it will take your cup, place it
    onto the coffee maker, and then when done swap out the empty cup for a
    full cup.

--- Assets/Scripts/ObjectInteraction/Interactables/CoffeeMaker.cs
+++ Assets/Scripts/ObjectInteraction/Interactables/CoffeeMaker.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class CoffeeMaker : MonoBehaviour, IInteractable
@@ -7,9 +8,12 @@ public class CoffeeMaker : MonoBehaviour, IInteractable
     [SerializeField] private GameObject fullCupPrefab;
     [SerializeField] private InteractableSettingsSO Settings;
     [SerializeField] private int numberOfUses = 1;
+    [SerializeField] private float transitionTime = .5f;
+    [SerializeField] private Transform coffeeLocationBottom;
     private GameObject currentItemHeld;
     private GameObject lastItemheld;
     private Outline outline;
+    private bool isPlacingCoffee;
 
     private void Awake()
     {
@@ -35,7 +39,7 @@ public class CoffeeMaker : MonoBehaviour, IInteractable
 
     public bool CanInteract()
     {
-        if (currentItemHeld != null && numberOfUses > 0)
+        if (currentItemHeld != null && numberOfUses > 0 && !isPlacingCoffee)
         {
             return requiredItem.Equals(currentItemHeld.name);
         }
@@ -47,13 +51,12 @@ public class CoffeeMaker : MonoBehaviour, IInteractable
     {
         stopInteraction.RaiseEvent();
 
-        GameObject fullCup = Instantiate(fullCupPrefab, lastItemheld.transform.position, lastItemheld.transform.rotation);
-        fullCup.transform.localScale = lastItemheld.transform.localScale;
-        fullCup.name = fullCupPrefab.name;
+        float cupHeight = lastItemheld.GetComponent<MeshRenderer>().bounds.extents.y;
+        Vector3 cupOffset = new(0f, cupHeight, 0f);
+        Vector3 coffeeLocation = coffeeLocationBottom.transform.position + cupOffset;
 
-        AudioManager.Instance.PlaySound(MixerType.SFX, SoundType.CoffeeBrew, 1f, transform.position);
-
-        Destroy(lastItemheld);
+        isPlacingCoffee = true;
+        StartCoroutine(MoveCoffeeToMachine(lastItemheld, coffeeLocation));
 
         numberOfUses--;
     }
@@ -80,5 +83,51 @@ public class CoffeeMaker : MonoBehaviour, IInteractable
         {
             lastItemheld = currentItemHeld;
         }
+    }
+
+
+
+    private IEnumerator MoveCoffeeToMachine(GameObject emptyCup, Vector3 location)
+    {
+        // Reduce empty cup to just mesh renderer.
+        Component[] components = emptyCup.transform.GetComponents<Component>();
+        foreach (Component component in components)
+        {
+            if (component is not Transform && component is not MeshFilter && component is not MeshRenderer)
+            {
+                Destroy(component);
+            }
+        }
+        while (emptyCup.transform.childCount > 0)
+        {
+            Destroy(emptyCup.transform.GetChild(0).gameObject);
+            yield return null;
+        }
+ 
+        emptyCup.transform.GetPositionAndRotation(out Vector3 startPosition, out Quaternion startRotation);
+        float timer = 0f;
+        while (timer < transitionTime)
+        {
+            Vector3 newPosition = Vector3.Slerp(startPosition, location, timer / transitionTime);
+            Quaternion newRotation = Quaternion.Slerp(startRotation, Quaternion.identity, timer / transitionTime);
+
+            emptyCup.transform.SetPositionAndRotation(newPosition, newRotation);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        emptyCup.transform.SetPositionAndRotation(location, Quaternion.identity);
+
+        // Waits until the audio is done playing. Manual number since audio manager doesn't support this.
+        AudioManager.Instance.PlaySound(MixerType.SFX, SoundType.CoffeeBrew, 1f, transform.position);
+        yield return new WaitForSeconds(2.5f);
+
+        GameObject fullCup = Instantiate(fullCupPrefab, lastItemheld.transform.position, lastItemheld.transform.rotation);
+        fullCup.transform.localScale = lastItemheld.transform.localScale;
+        fullCup.name = fullCupPrefab.name;
+
+        Destroy(lastItemheld);
+
+        isPlacingCoffee = false;
     }
 }
