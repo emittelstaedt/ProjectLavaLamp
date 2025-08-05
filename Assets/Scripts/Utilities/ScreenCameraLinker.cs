97e0298bfda6830280cc9bd2bd65aa59ae2de701
AmandaBoard <boar9227@vandals.uidaho.edu>
2025-08-04 17:17:42 -0700

    Add/Remove Scripts to Terminal Prefabs
    I added the ScreenTerminalLinker script to all terminal prefabs so other
    people will not struggle on the aspect ratio for other minigames. In
    making the CursorController abstract, it now gives warnings whenever it
    is attached to game objects, so I made sure to remove the
    cursorController script from the terminals to help avoid some of those
    warnings, especially for our main scene. I did not remove the
    cursorcontroller scripts from the test scenes.

---
+++ Assets/Scripts/Utilities/ScreenCameraLinker.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[RequireComponent(typeof(Renderer))]
+public class ScreenCameraLinker : MonoBehaviour
+{
+    [Tooltip("The camera to render to the screen.")]
+    [SerializeField] new private Camera camera;
+    [Tooltip("The screen height in pixels.")]
+    [SerializeField] private int screenHeight = 500;
+    [SerializeField] private string materialPath = "Assets/Art/Materials/";
+    [SerializeField] private string renderTexturePath = "Assets/Art/Textures/";
+    [SerializeField] new private string name;
+
+    [ContextMenu("Link camera to screen")]
+    private void LinkCameraToScreen()
+    {
+        int screenWidth = (int) (screenHeight * (transform.lossyScale.x / transform.lossyScale.y));
+
+        Renderer renderer = GetComponent<Renderer>();
+        RenderTexture newRenderTexture = new(screenWidth, screenHeight, 24);
+        Material newMaterial = new(Shader.Find("Universal Render Pipeline/Lit"));
+
+        string fullRenderTexturePath = GetFullPath(renderTexturePath, ".renderTexture");
+        string fullMaterialPath = GetFullPath(materialPath, ".mat");
+
+        if (File.Exists(fullRenderTexturePath))
+        {
+            Debug.LogError(fullRenderTexturePath + " already exists.");
+            return;
+        }
+
+        if (File.Exists(fullMaterialPath))
+        {
+            Debug.LogError(fullMaterialPath + " already exists.");
+            return;
+        }
+
+        AssetDatabase.CreateAsset(newMaterial, fullMaterialPath);
+        AssetDatabase.CreateAsset(newRenderTexture, fullRenderTexturePath);
+        AssetDatabase.SaveAssets();
+
+        newMaterial.mainTexture = newRenderTexture;
+        camera.targetTexture = newRenderTexture;
+        renderer.material = newMaterial;
+
+        Debug.Log("New material created at: " + fullMaterialPath);
+        Debug.Log("New render texture created at: " + fullRenderTexturePath);
+    }
+
+    [ContextMenu("Resync Ratio")]
+    private void SyncRenderTextureRatio()
+    {
+        string fullRenderTexturePath = GetFullPath(renderTexturePath, ".renderTexture");
+        string fullMaterialPath = GetFullPath(materialPath, ".mat");
+        
+        if (File.Exists(fullRenderTexturePath) && File.Exists(fullMaterialPath))
+        {
+            AssetDatabase.DeleteAsset(fullRenderTexturePath);
+            AssetDatabase.DeleteAsset(fullMaterialPath);
+            Debug.Log("Deleting " + fullRenderTexturePath);
+            Debug.Log("Deleting " + fullMaterialPath);
+
+            LinkCameraToScreen();
+        }
+        else
+        {
+            Debug.LogError("Resync failed. Please manually delete the material and render texture, then link the camera to the screen.");
+        }
+    }
+
+    private string GetFullPath(string path, string extension)
+    {
+        if (path[path.Length - 1] != '/')
+        {
+            path += '/';
+        }
+
+        if (name == null || name == "")
+        {
+            return path + transform.parent.name + extension;
+        }
+        else
+        {
+            return path + name + extension;
+        }
+    }
+}
