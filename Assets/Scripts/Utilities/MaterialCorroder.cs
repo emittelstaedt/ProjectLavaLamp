027e03e42b5325b8a19c27973225557c5bfcec76
Aiden Shepard <aidenL.shepard@gmail.com>
2025-08-07 08:59:23 -0700

    Create script to swap corrode levels
    A script can now be attached to all items that change materials to a
    clean, corrode, or ultra corrode. It saves the given materials in a
    dictionary and only swap to the highest level closest to whichever one
    is set. So if you set to ultra corrode and it doesn't have one it will
    set the corrode instead.

---
+++ Assets/Scripts/Utilities/MaterialCorroder.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CorrodeLevel
+{
+    Clean,
+    Corrode,
+    UltraCorrode,
+}
+
+[RequireComponent(typeof(Renderer))]
+public class MaterialCorroder : MonoBehaviour
+{
+    [SerializeField] private Material cleanMaterial;
+    [SerializeField] private Material corrodeMaterial;
+    [SerializeField] private Material ultraCorrodeMaterial;
+    private readonly Dictionary<CorrodeLevel, Material> materialList;
+    private Renderer materialRenderer;
+    
+    private void Awake()
+    {
+        materialRenderer = GetComponent<Renderer>();
+
+        if (cleanMaterial != null)
+        {
+            materialList.Add(CorrodeLevel.Clean, cleanMaterial);
+        }
+        if (corrodeMaterial != null)
+        {
+            materialList.Add(CorrodeLevel.Corrode, corrodeMaterial);
+        }
+        if (ultraCorrodeMaterial != null)
+        {
+            materialList.Add(CorrodeLevel.UltraCorrode, ultraCorrodeMaterial);
+        }
+    }
+
+    public void SetMaterial(int level)
+    {
+        CorrodeLevel corrodeLevel = (CorrodeLevel) level;
+
+        if (materialList.TryGetValue(corrodeLevel, out Material material))
+        {
+            materialRenderer.material = material;
+        }
+        else if (level > 0)
+        {
+            SetMaterial(level - 1);
+        }
+    }
+}
