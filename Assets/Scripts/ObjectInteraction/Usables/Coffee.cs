d44e2b2eed51ba093faace5237ed7682f7a4527b
Lightningluster <82775417+Lightningluster@users.noreply.github.com>
2026-06-18 15:31:06 -0700

    Made the main room door locked until coffee is drank
    Gave the full coffee cup script a signal it emits when drank, gave doorMover script a serializefield bool "unlocked" that can be called via signal, applied these to officeworkplace scene.

--- Assets/Scripts/ObjectInteraction/Usables/Coffee.cs
+++ Assets/Scripts/ObjectInteraction/Usables/Coffee.cs
@@ -5,12 +5,14 @@ public class Coffee : MonoBehaviour, IUsable
 {
     [SerializeField] private VoidEventChannelSO stopInteraction;
     [SerializeField] private IntEventChannelSO changePostProcess;
+    [SerializeField] private VoidEventChannelSO coffeeDrank;
     [SerializeField] private GameObject emptyCupPrefab;
 
     public void UseItem()
     {
         stopInteraction.RaiseEvent();
         changePostProcess.RaiseEvent((int) PostProcess.Clean);
+        coffeeDrank.RaiseEvent();
 
         GameObject emptyCup = Instantiate(emptyCupPrefab, transform.position, transform.rotation);
         emptyCup.transform.localScale = transform.localScale;
