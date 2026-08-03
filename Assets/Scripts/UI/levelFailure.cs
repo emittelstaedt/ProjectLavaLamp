656ca3439cb7222aef9f53ac04e4878e235bce17
Lightningluster <82775417+Lightningluster@users.noreply.github.com>
2026-08-03 10:08:33 -0700

    Endgame noise and volume fix

--- Assets/Scripts/UI/levelFailure.cs
+++ Assets/Scripts/UI/levelFailure.cs
@@ -92,13 +92,13 @@ public class levelFailure : MonoBehaviour
 	
 	public void PressMainMenuButton()
 	{
-		AudioManager.Instance.PlaySound(MixerType.SFX, SoundType.MenuClick, 1f);
+		AudioManager.Instance.PlaySound(MixerType.SFX, SoundType.MenuClick, 0.5f);
 		triggerMainMenu.RaiseEvent();
 	}
 	
 	public void PressRetryButton()
 	{
-		AudioManager.Instance.PlaySound(MixerType.SFX, SoundType.MenuClick, 1f);
+		AudioManager.Instance.PlaySound(MixerType.SFX, SoundType.MenuClick, 0.5f);
 		startGame.RaiseEvent();
 		triggerNextLevel.RaiseEvent();
 	}
